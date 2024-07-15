5b1c0109293fb15c86e6bf8823215a43b59fcba7
vikash-kanauja <vikashkanoujia@gmail.com>
2024-07-15 11:54:05 +0530

    Project setup complete

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import jwt from "jsonwebtoken"
-import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -50,42 +50,42 @@ const userSchema = new Schema(
   }
 );
 
-userSchema.pre("save", async function(next){
-  if(!this.isModified("password")) return next();
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password, 10)
-    next()
-})
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
 
-userSchema.methods.isPasswordCorrect = async function(password){
-  return await bcrypt.compare(password,this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
-      email:this.email,
-      username:this.username,
-      fullname:this.fullname
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
     },
     process.env.ACCESS_TAKEN_SECRET,
     {
-      expiresIn:process.env.ACCESS_TAKEN_EXPIRY 
+      expiresIn: process.env.ACCESS_TAKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 
-userSchema.methods.generateAccessToken = function(){
+userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     {
       _id: this._id,
     },
     process.env.REFRESH_TAKEN_SECRET,
     {
-      expiresIn:process.env.REFRESH_TAKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TAKEN_EXPIRY,
     }
-  )
-}
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const VideoSchema = new Schema(
   {
     videoFile: {
@@ -40,5 +40,5 @@ const VideoSchema = new Schema(
   }
 );
 
-VideoSchema.plugin(mongooseAggregatePaginate)
+VideoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", VideoSchema);
